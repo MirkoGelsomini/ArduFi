42830904353a20d916d96b82c46c8947b6cceb88
Fabiano Riccardi <fabiuz4@hotmail.it>
2018-02-28 16:40:06 +0100

    First working sketch, the set and get api are implemented
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,73 +1,103 @@
-#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <DNSServer.h>
 #include <WiFiManager.h>
 
-#define TRIGGER_PIN D4 //(D4=2, only 0 and 2 work, always set them HIGH at power-up. If the pin is low, the config manager starts)
+ESP8266WebServer server(80);
+
+const int led = LED_BUILTIN;
+
+int myData = -1;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Led configuration
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Serial configuration
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  delay(1000);
-  Serial.print("IP:");
-  Serial.println(WiFi.localIP());
-  //Serial.println(WiFi.gatewayIP());
-  //Serial.println(WiFi.subnetMask());
+  // Wifi configuration
+  Serial.println("------------ WiFiManager ---------");
+  
+  WiFiManager wifiManager; //Local initialization of WiFiManager
+  //wifiManager.resetSettings(); //reset settings - for testing
+  if (!wifiManager.autoConnect("OnDemandAP")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000); 
+    ESP.reset(); //reset and try again, or maybe put it to deep sleep
+    delay(5000);
+  }
+  Serial.println("Connected");
 
-  pinMode(TRIGGER_PIN, INPUT);
+  // HTTP configuration
+  server.on("/", handleRoot);
+  server.on("/info", info);
+  server.on("/getData", getData);
+  server.on("/setData", setData);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
 
+  // put your setup code here, to run once:
 }
 
-
 void loop() {
-  
-  if (digitalRead(TRIGGER_PIN) == LOW) // is configuration portal requested?
-  {
-    Serial.println("------------ WiFiManager ---------");
-
-    WiFiManager wifiManager; //Local initialization of WiFiManager
-    //wifiManager.resetSettings(); //reset settings - for testing
-   if (!wifiManager.startConfigPortal("OnDemandAP")) {
-      Serial.println("failed to connect and hit timeout");
-      delay(3000); 
-      ESP.reset(); //reset and try again, or maybe put it to deep sleep
-      delay(5000);
-    }
-
-    Serial.println("Connected");
-    Serial.print("IP:");
-    Serial.println(WiFi.localIP());
-    //Serial.println(WiFi.gatewayIP());
-    //Serial.println(WiFi.subnetMask());
-  }
-
-  
-  /*WiFiClient client = WiFi.getClient();
-  if (!client) {
-    return;
-  }
-
-    //Recheck if the client is really available
-  int temp=0;
-  while(!client.available()){
-    delay(1);
-    temp+=1;
-    if(temp==1000){
-      Serial.println("Client not available: aborted");
-      return;
-    }
-  }
-  
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.print("Client request:");
-  Serial.println(request);
-  String path = request.substring(request.indexOf("GET ")+4,request.indexOf(" HTTP/1.1"));
-  Serial.print("Path: ");
-  Serial.println(path);*/
-
   // put your main code here, to run repeatedly:
 
+  // Ask to the Wemos if there is a pending request
+  server.handleClient();
+}
+
+void handleRoot() {
+  digitalWrite(led, LOW);
+  server.send(200, "text/plain", "Hello world, from esp8266!");
+  digitalWrite(led, HIGH);
+}
+
+void info() {
+  digitalWrite(led, LOW);
+  int seconds=(millis()/1000);
+  server.send(200, "text/html", String("<html><body><b>Info</b><br/><br/>I'm connected to ") + WiFi.SSID() + 
+  "<br/>My I address is " + WiFi.localIP().toString() +
+  "<br/><br/>I was turned on " + seconds + " seconds ago</body></html>");
+  digitalWrite(led, HIGH);
+}
+
+void getData(){
+  digitalWrite(led, LOW);
+  server.send(200, "text/plain", String(myData));
+  digitalWrite(led, HIGH);
+}
+
+void setData(){
+  digitalWrite(led, LOW);
+  String key=server.argName(0);
+  String value=server.arg(0);
+  
+  String message;
+  if(key=="data"){
+    myData=value.toInt();
+    Serial.println(String("New data: ") + myData);
+    message= "ok";
+  }else{
+    message= "error";
+  }
+  server.send(200, "text/plain", message);
+  digitalWrite(led, HIGH);
+}
+
+void handleNotFound(){
+  digitalWrite(led, LOW);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, HIGH);
 }
